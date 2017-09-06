97806424a129f54934a1881299e5c2aa804bb5ce
RLH <superrab@gmail.com>
2017-09-06 13:42:41 -0700

    Completed Routing tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
     providers: [],
 })
 export class AppComponent {
